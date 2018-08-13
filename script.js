7685d05e888f4ed12b5a9783d2df4d990a8d0e1e
jujuandkarma <bagsjosh7@gmail.com>
2018-08-13 16:50:56 -0700

    buttons working

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 
 var resultBox = document.getElementById('result')
 var titleBox = document.getElementById('title')
-var voteBox = document.getElementById('vote-box')
+var gifPanel = document.getElementById('gif-panel')
 var mainImage = document.getElementById('main-image')
 var youImage = document.getElementById('you-image')
 var spotifyImage = document.getElementById('spotify-image')
@@ -28,15 +28,57 @@ var spotifyScore = document.getElementById('spotify-score')
 var yrScore = document.getElementById('yr-score')
 
 // this is how you replace the image from the background
-mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
 
 // this is how we hide the resultBox element
 resultBox.hidden = true;
 
 // this is how you create a function
-function testClickFunction(){
-  alert('clickedddd')
+var userRating = 0;
+
+function button1(){
+  userRating = 1
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)"
 }
+function button2(){
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
+}
+function button3(){
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
+}
+function button4(){
+  userRating=4
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif)"
+}
+function button5(){
+  userRating=5
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif)"
+}
+function button6(){
+  userRating=6
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif)"
+}
+function button7(){
+  userRating=7
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif)"
+}
+function button8(){
+  userRating = 8
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)"
+}
+function button9(){
+  userRating =9
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)"
+}
+function button10(){
+  userRating = 10
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)"
+}
+  function compare() {
+    resultBox.hidden = false;
+    //gifPanel.hidden = true;
+  }
+
+
 
 // Activity today will be
 
